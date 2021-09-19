897728f0ffaa6360c322dc44c1057ec52b7752aa
Victor <asaptorvi@protonmail.com>
2021-09-19 16:06:24 -0300

    feat: use nexus instead of graphqljs

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,8 +1,14 @@
-import { GraphQLSchema } from 'graphql';
-import query from './query';
+import { makeSchema } from 'nexus';
+import Query from './query';
+import UserType from '../modules/user/user.type';
+import CreateUserMutation from '../modules/user/mutations/create-user.mutation';
 
-const schema = new GraphQLSchema({
-  query,
+const schema = makeSchema({
+  types: [Query, UserType, CreateUserMutation],
+  outputs: {
+    schema: __dirname + '/generated/schema.graphql',
+    typegen: __dirname + '/generated/typings.ts',
+  },
 });
 
 export default schema;

1c339846ffb0c362d6475cb70a99f79f5e5aded5
Victor <asaptorvi@protonmail.com>
2021-10-08 19:06:42 -0300

    feat: process env declaration type

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 import 'module-alias/register';
 import fastify from 'fastify';
+import fastifyEnv from 'fastify-env';
 import AltairFastify from 'altair-fastify-plugin';
 import mercurius from 'mercurius';
 import schema from './graphql';
 import buildContext from './graphql/context';
 import databasePlugin from './plugins/database';
 
+const envSchema = {
+  type: 'object',
+  required: ['JWT_SECRET', 'JWT_EXPIRE'],
+  properties: {
+    JWT_SECRET: {
+      type: 'string',
+    },
+    JWT_EXPIRE: {
+      type: 'string',
+    },
+  },
+};
+
 const app = fastify({ logger: true });
 
+app.register(fastifyEnv, { schema: envSchema, dotenv: true });
 app.register(databasePlugin);
 
 app.register(mercurius, {

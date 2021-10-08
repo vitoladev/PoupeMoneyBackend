7c412aa4819cb85966c4cb0e0d3eb55a40acd6b5
Victor <asaptorvi@protonmail.com>
2021-10-07 23:38:25 -0300

    feat: typescript module resolver

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,5 +1,5 @@
-import schema from '../src/graphql';
 import { ExecutionResult, graphql } from 'graphql';
+import schema from '../src/graphql';
 
 export const executeGraphQL = (
   query: string,
--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,11 @@ export default {
   restoreMocks: true,
   clearMocks: true,
   roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^@core(.*)$': '<rootDir>/src/core$1',
+    '^@graphql(.*)$': '<rootDir>/src/graphql$1',
+    '^@modules(.*)$': '<rootDir>/src/modules$1',
+  },
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).+(ts)'],
   testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/build/'],
   transform: {
